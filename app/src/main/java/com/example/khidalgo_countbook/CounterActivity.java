cc2b498a5170fa9531788fbbb46e167a54758367
hidalgok <kenchristopherhidalgo@gmail.com>
2017-10-01 21:45:32 -0600

    created some layouts

---
+++ app/src/main/java/com/example/khidalgo_countbook/CounterActivity.java
@@ -0,0 +1,12 @@
+package com.example.khidalgo_countbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CounterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/com/example/khidalgo_countbook/MainActivity.java
+++ app/src/main/java/com/example/khidalgo_countbook/MainActivity.java
@@ -2,9 +2,18 @@ package com.example.khidalgo_countbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String FILENAME = "file.sav";
+    private ListView oldCounterList;
+
+    private ArrayList<Counter> counterList;
+    private ArrayAdapter<Counter> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
